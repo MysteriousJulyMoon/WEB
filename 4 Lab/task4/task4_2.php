699e8204938229fb55f319ad6c03cf13d3646510
Julia <150614653+MysteriousJulyMoon@users.noreply.github.com>
2025-04-03 21:02:50 +0200

    Update task4_2.php

--- 4 Lab/task4/task4_2.php
+++ 4 Lab/task4/task4_2.php
@@ -1,10 +1,14 @@
 <?php
 session_start();
-if (!isset(\$_SESSION["dish"])) {
+
+if (!isset($_SESSION["dish"])) {
     header("Location: task4_1.php");
     exit();
 }
-echo "Блюдо: " . htmlspecialchars(\$_SESSION["dish"]) . "<br>";
-echo "Ингредиенты: " . htmlspecialchars(\$_SESSION["ingredients"]) . "<br>";
-echo "Время: " . htmlspecialchars(\$_SESSION["time"]) . "₽";
+
+echo "<h3>Информация о блюде:</h3>";
+echo "Название блюда: " . htmlspecialchars($_SESSION["dish"]) . "<br>";
+echo "Ингредиенты: " . htmlspecialchars($_SESSION["ingredients"]) . "<br>";
+echo "Время приготовления: " . htmlspecialchars($_SESSION["time"]) . " минут";
 ?>
+
